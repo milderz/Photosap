9766f1016c110c1c673efa0edc4abb18b427ad2e
milderz <mzunax@gmail.com>
2022-11-09 17:46:25 -0600

    Added hover effect to the nav links

--- src/Components/Styles/Navbar.styled.js
+++ src/Components/Styles/Navbar.styled.js
@@ -9,16 +9,29 @@ export const StyledNavBar = styled.nav`
     display: flex;
 
     li {
-      padding: 0 2rem;
+      margin: 0 2rem;
+      position: relative;
       a {
         color: ${({ theme }) => theme.colors.black};
         font-size: 1.2rem;
         letter-spacing: 0.19em;
         font-weight: 600;
-        transition: color 0.4s ease;
 
-        :hover {
-          color: #737373;
+        ::after {
+          content: "";
+          width: 100%;
+          height: 0.2rem;
+          background: linear-gradient(to right, #5a77ff, #bc7198, #ffc593);
+          bottom: 0;
+          left: 0;
+          position: absolute;
+          z-index: 100;
+          opacity: 0;
+          transition: opacity 0.4s ease;
+        }
+
+        :hover::after {
+          opacity: 1;
         }
       }
     }
